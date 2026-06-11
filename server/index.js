463e7378aad2a01da7e9033bcb58ca4aad912479
saipradeeptlk-ally <saipradeeptlk@gmail.com>
2026-06-11 14:12:10 +0530

    Fix: update socket.io cors for production

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app)
 
-const io = new Server(server, { cors: { origin: 'http://localhost:3000' } })
+const io = new Server(server, { cors: { origin: 'https://snazzy-lamington-70183a.netlify.app' } })
 
 io.on('connection', (socket) => {
   socket.on('join-room', (room) => {
